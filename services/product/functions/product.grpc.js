690b3e9de841be67d9914196517efbc0d811dd11
navid bahrami <navidbahrami1996@gmail.com>
2024-12-14 21:35:39 +0330

    product grpc

---
+++ services/product/functions/product.grpc.js
@@ -0,0 +1,16 @@
+async function listProduct(call,callback){}
+async function getProduct(call,callback){}
+async function createProduct(call,callback){}
+async function updateProduct(call,callback){}
+async function deleteProduct(call,callback){}
+
+
+
+
+module.exports = {
+    listProduct ,
+    getProduct ,
+    createProduct ,
+    updateProduct ,
+    deleteProduct
+}
