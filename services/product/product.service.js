d60a4a40606d29ab8044cf6ef61ea2e4c36826ff
navid bahrami <navidbahrami1996@gmail.com>
2024-12-14 21:35:16 +0330

    product service

--- services/product/product.service.js
+++ services/product/product.service.js
@@ -1,4 +1,12 @@
 require('./config/db.connection')
 const grpc = require('@grpc/grpc-js')
-const grpcLoader = require('@grpc/proto-loader')
+const protoLoader = require('@grpc/proto-loader')
 const path = require('path')
+const protoPath = path.join(__dirname , '..' , '..' , 'protos' , 'product.proto');
+const productProto = protoLoader.loadSync(protoPath);
+const {productPackage} = grpc.loadPackageDefinition(productProto)
+const productServiceURL = 'localhost:4001';
+function main(){
+    const server = new grpc.Server()
+    server.addService(productPackage.ProductService.service)
+}
