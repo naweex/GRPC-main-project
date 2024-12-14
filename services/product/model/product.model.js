a547fc27e8b85df9a602df03d9d2b9871846cc7d
navid bahrami <navidbahrami1996@gmail.com>
2024-12-14 19:02:37 +0330

    product model

--- services/product/model/product.model.js
+++ services/product/model/product.model.js
@@ -1,6 +1,19 @@
 const { default: mongoose } = require("mongoose");
 
 const ProductSchema = new mongoose.Schema({
-    
-
+    id: {type: Number},
+    title: {type: String},
+    price: {type: String}
 })
+ProductSchema.pre("save", function(next) {
+    const self = this;
+    self.constructor.count(async function(err, data) {
+        if(err) return next(err);
+        self.set({id: (data + 1)})
+        next();
+    })
+})
+
+module.exports = {
+    ProductModel : mongoose.model('product' , ProductSchema)
+}
