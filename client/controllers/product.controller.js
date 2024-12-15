0e5f5348105444d738ed8e6b8e69e2933c1694ef
navid bahrami <navidbahrami1996@gmail.com>
2024-12-15 12:33:12 +0330

    product controller

--- client/controllers/product.controller.js
+++ client/controllers/product.controller.js
@@ -5,3 +5,10 @@ const productProtoPath = path.join(__dirname , '..' , 'protos' , 'product.proto'
 const productProto = protoLoader.loadSync(protoPath);
 const {productPackage} = grpc.loadPackageDefinition(productProto)
 const productServiceURL = 'localhost:4001';
+const productClient = new productPackage.ProductService(productServiceURL, grpc.credentials.createInsecure());
+async function listProduct(req , res , next){}
+async function getProduct(req , res , next){}
+async function createProduct(req , res , next){}
+async function updateProduct(req , res , next){}
+async function deleteProduct(req , res , next){}
+
