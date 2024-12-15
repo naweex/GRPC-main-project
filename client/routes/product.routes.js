74f8b37934855ce6a3dc3e4e50be28eb33823965
navid bahrami <navidbahrami1996@gmail.com>
2024-12-15 17:42:24 +0330

    product router

--- client/routes/product.routes.js
+++ client/routes/product.routes.js
@@ -1,5 +1,11 @@
+const { listProduct, getProduct, deleteProduct, updateProduct, createProduct } = require('./../controllers/product.controller');
 const router = require('express').Router();
 
+router.get('/list' , listProduct)
+router.get('/create' , createProduct)
+router.get('/update' , updateProduct)
+router.get('/delete/:id' , deleteProduct)
+router.get('/:id' , getProduct)
 module.exports = {
     ProductRouter : router
 }
