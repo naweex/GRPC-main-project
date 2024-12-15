bd3c939cdc3400233a1badd1b0e595d012f717fe
navid bahrami <navidbahrami1996@gmail.com>
2024-12-15 17:31:29 +0330

    product controller

--- client/controllers/product.controller.js
+++ client/controllers/product.controller.js
@@ -1,14 +1,26 @@
 const grpc = require('@grpc/grpc-js')
 const protoLoader = require('@grpc/proto-loader')
 const path = require('path');
-const productProtoPath = path.join(__dirname , '..' , 'protos' , 'product.proto');
-const productProto = protoLoader.loadSync(protoPath);
+const productProtoPath = path.join(__dirname , '..' , '..' , 'protos' , 'product.proto');
+const productProto = protoLoader.loadSync(productProtoPath);
 const {productPackage} = grpc.loadPackageDefinition(productProto)
 const productServiceURL = 'localhost:4001';
 const productClient = new productPackage.ProductService(productServiceURL, grpc.credentials.createInsecure());
-async function listProduct(req , res , next){}
+async function listProduct(req , res , next){
+    productClient.listProduct(null , (err , data) => {
+        if(err) return res.json(err);
+        return res.json(data)
+    })
+}
 async function getProduct(req , res , next){}
 async function createProduct(req , res , next){}
 async function updateProduct(req , res , next){}
 async function deleteProduct(req , res , next){}
 
+module.exports = {
+    listProduct ,
+    getProduct ,
+    createProduct ,
+    updateProduct ,
+    deleteProduct
+}
