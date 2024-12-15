fd0b861e277e05c04e8bab3dacd1c69a613089a5
navid bahrami <navidbahrami1996@gmail.com>
2024-12-15 17:48:54 +0330

    product controller

--- client/controllers/product.controller.js
+++ client/controllers/product.controller.js
@@ -6,17 +6,40 @@ const productProto = protoLoader.loadSync(productProtoPath);
 const {productPackage} = grpc.loadPackageDefinition(productProto)
 const productServiceURL = 'localhost:4001';
 const productClient = new productPackage.ProductService(productServiceURL, grpc.credentials.createInsecure());
-async function listProduct(req , res , next){
+function listProduct(req , res , next){
     productClient.listProduct(null , (err , data) => {
         if(err) return res.json(err);
         return res.json(data)
     })
 }
-async function getProduct(req , res , next){}
-async function createProduct(req , res , next){}
-async function updateProduct(req , res , next){}
-async function deleteProduct(req , res , next){}
-
+function getProduct(req, res, next) {
+    const {id} = req.params;
+    productClient.getProduct({id}, (err, data) => {
+        if(err) return res.json(err);
+        return res.json(data)
+    })
+}
+function createProduct(req, res, next) {
+    const {title, price} = req.query;
+    productClient.createProduct({title, price}, (err, data) => {
+        if(err) return res.json(err);
+        return res.json(data)
+    })
+}
+function updateProduct(req, res, next) {
+    const data = req.query;
+    productClient.updateProduct(data, (err, data) => {
+        if(err) return res.json(err);
+        return res.json(data)
+    })
+}
+function deleteProduct(req, res, next) {
+    const {id} = req.params;
+    productClient.deleteProduct({id}, (err, data) => {
+        if(err) return res.json(err);
+        return res.json(data)
+    })
+}
 module.exports = {
     listProduct ,
     getProduct ,
